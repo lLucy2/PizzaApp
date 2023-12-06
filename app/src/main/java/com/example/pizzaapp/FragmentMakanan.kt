285622c82dfdbb31273756aa9c7dd0a68129e9f6
lLucy2 <raydamellos@gmail.com>
2023-12-06 15:58:40 +0700

    UDPATE

--- app/src/main/java/com/example/pizzaapp/FragmentMakanan.kt
+++ app/src/main/java/com/example/pizzaapp/FragmentMakanan.kt
@@ -5,8 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+
 import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
